934d97f8c18f8cf433fa8c8a36b10111b85e6076
jasonandmonte <jason.monte55@gmail.com>
2023-06-06 13:47:53 -0700

    Add cross-origin requests

--- 07_frontend/main.py
+++ 07_frontend/main.py
@@ -2,10 +2,22 @@
 
 from xkcdb import XKCDb
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-
+# uvicorn main:app --reload
 app = FastAPI()
 db = XKCDb("xkcd.json")
+
+# NOTE: Hack to allow me to use fetch from a different "origin".
+# ref: 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 # base path "home page"
